f760a772540c7d6e01bf4eab3451cc6853abcf72
Sahil Gupta <sahilgupta8995@gmail.com>
2021-10-01 10:13:27 +0530

    Using useState to output lists

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 function Home() {
+  const [blogs, setBlogs] = useState([
+    { title: "Blog1", body: "This is the blog 1", id: 1 },
+    { title: "Blog2", body: "This is the blog 2", id: 2 },
+    { title: "Blog3", body: "This is the blog 3", id: 3 },
+    { title: "Blog4", body: "This is the blog 4", id: 4 },
+  ]);
   return (
     <div>
-      <h1>Hompage</h1>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          {blog.title}
+          {blog.body}
+        </div>
+      ))}
     </div>
   );
 }
